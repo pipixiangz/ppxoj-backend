319405c6d3f146786febd6bb217355e86b314f37
pipixiangz <763038134@qq.com>
2024-07-22 16:23:16 +0800

    给代码沙箱提供API并保证安全

--- src/main/java/com/ppx/ppxoj/judge/codesandbox/impl/RemoteCodeSandbox.java
+++ src/main/java/com/ppx/ppxoj/judge/codesandbox/impl/RemoteCodeSandbox.java
@@ -1,10 +1,20 @@
 package com.ppx.ppxoj.judge.codesandbox.impl;
 
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
+import com.ppx.ppxoj.common.ErrorCode;
+import com.ppx.ppxoj.exception.BusinessException;
 import com.ppx.ppxoj.judge.codesandbox.CodeSandbox;
 import com.ppx.ppxoj.judge.codesandbox.model.ExecuteCodeRequest;
 import com.ppx.ppxoj.judge.codesandbox.model.ExecuteCodeResponse;
+import org.apache.commons.lang3.StringUtils;
 
 public class RemoteCodeSandbox implements CodeSandbox {
+    // 定义鉴权请求头和密钥
+    private static final String AUTH_REQUEST_HEADER = "auth";
+
+    private static final String AUTH_REQUEST_SECRET = "secretKey";
+
     /**
      * 远程代码沙箱（实际调用接口的沙箱）
      *
@@ -13,8 +23,17 @@ public class RemoteCodeSandbox implements CodeSandbox {
      */
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
-        System.out.println("远程代码沙箱");
-        return null;
+        String url = "http://localhost:8090/executeCode";
+        String json = JSONUtil.toJsonStr(executeCodeRequest);
+        String responseStr = HttpUtil.createPost(url)
+                .header(AUTH_REQUEST_HEADER, AUTH_REQUEST_SECRET) // 基本的安全认证
+                .body(json)
+                .execute()
+                .body();
+        if (StringUtils.isBlank(responseStr)) {
+            throw new BusinessException(ErrorCode.API_REQUEST_ERROR, "executeCode remoteSandbox error, message = " + responseStr);
+        }
+        return JSONUtil.toBean(responseStr, ExecuteCodeResponse.class);
     }
 
 }
